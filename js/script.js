71b9702e60cd46f7de45b3fa6b7356808117f97c
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2023-12-20 21:23:28 +0100

    create am element on html, and added content from js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
 console.log('JS OK')
-/*-1 Faccio partire un ciclo per stampare in console i numeri da 1 a 100
--2 In base al numero sostituisco con if il valore della i.
+/*
+-1 Aggiungo eventuali variabili fisse
+-2 inizio il ciclo per aggiungere alla console un conteggio da 1-100
+-3 Aggiungo una variabile a cui do lo stesso valore del numero dei cicli
+-4 Cambio il valore della variabile in base alla sua divisibilità
+-5 Creo una variabile li a cui aggiungo la variabile number
+-6 Aggiungo la variabile li a elemento fisso ul
+-7 stampo tutto in pagina 
+
 
 
 
 */
+//VARIABILI FISSE
+const element = document.getElementById('target')
+const list = document.createElement('ul')
 
 
-console.log(i)
+
+//INIZIO CICLO con cambio valori 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz')
+    let number = i;
+    if (number % 3 === 0 && number % 5 === 0) {
+        number = ('FizzBuzz')
     }
-    else if (i % 5 === 0) {
-        console.log('Buzz')
+    else if (number % 3 === 0) {
+        number = ('Fizz')
+
     }
-    else if (i % 3 === 0) {
-        console.log('Fizz')
+    else if (number % 5 === 0) {
+        number = ('Buzz')
     }
-    else {
-        console.log(i)
-    }
+    listItem = document.createElement('li');
+    listItem.append(number);
+    list.appendChild(listItem)
+
 }
-
-
-
-
+//Tolto lo stile alla lista
+list.classList.add('list-unstyled');
+// Aggiuti i list item alla lista
+element.appendChild(list)
 
